65621ef2e15f988a0a711326ddad37bc385738e8
danielsantana9824 <danielsantana9824@gmail.com>
2024-09-12 22:35:17 -0500

    9/12/24  movibility card
    i fix the bus where i can't move the card between lines

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -52,7 +52,6 @@ function generateTaskId() {
     if (tasks.length > 0) {
         task.id = tasks.length + 1;
     } else {
-        // avoidLog = true;
         task.id = 1;
     }
 
@@ -62,89 +61,6 @@ function generateTaskId() {
     createTaskCard();
     cleanForm();
 }
-
-// Todo: create a function to create a task card
-// function createTaskCard() {
-
-//     const todoList = $('#todo-cards');
-//     todoList.empty();
-
-//     const inProgressList = $('#in-progress-cards');
-//     inProgressList.empty();
-
-//     const doneList = $('#done-cards');
-//     doneList.empty();
-
-//     const tasks = loadData();
-
-//     if (tasks.length > 0) {
-//         tasks.forEach(task => {
-
-//             const cardColumnEl = $('<div>');
-//             cardColumnEl
-//                 .addClass('col-12 col-sm-4 col-md-3 col-lg-4 m-1 dif')
-//                 .attr('data-project-id', task.id);
-
-//             const cardEl = $('<div>');
-//             cardEl.addClass('card h-100  toDo');
-
-//             if (task.date && task.status !== 'done') {
-//                 const now = dayjs();
-//                 const taskDueDate = dayjs(task.date, 'DD/MM/YYYY');
-
-//                 if (now.isSame(taskDueDate, 'day')) {
-//                     cardEl.addClass('bg-warning text-white');
-//                 } else if (now.isAfter(taskDueDate)) {
-//                     cardEl.addClass('bg-danger text-white');
-//                 }
-//             }
-
-//             cardEl.appendTo(cardColumnEl);
-
-//             const cardName = $('<h5>')
-//                 .addClass('card-header')
-//                 .text(task.title);
-//             cardName.appendTo(cardEl);
-
-//             const cardBodyEl = $('<div>');
-//             cardBodyEl.addClass('card-body');
-//             cardBodyEl.appendTo(cardEl);
-
-//             const cardDescription = $('<p>').addClass('card-text').text(task.description);
-//             cardDescription.appendTo(cardBodyEl);
-
-//             const cardDate = $('<p>').addClass('card-text date').text(task.date);
-//             cardDate.appendTo(cardBodyEl);
-
-//             const buttomDelete = $('<button>')
-//             buttomDelete.addClass('btn btn-danger delete')
-//                 .text("Delete")
-//                 .attr('data-project-id', task.id);
-//             buttomDelete.appendTo(cardBodyEl);
-            
-
-
-//             // const buttomDelete = $('<p>')
-//             //     .append('<button type="button" onclick="displayButtons(this)" class="btn btn-danger delete" >Delete</button>')
-//             //     .attr('data-project-id', task.id);
-//             //     buttomDelete.appendTo(cardBodyEl);
-
-
-
-//             if (task.status === "toDo") {
-//                 toDo.append(cardColumnEl);
-
-//             } else if (task.status === "in-progress") {
-//                 inProgress.append(cardColumnEl);
-//             } else {
-//                 doneCards.append(cardColumnEl);
-//             }
-
-//         });
-//     } else {
-//         avoidLog = true;
-//     }
-// }
 
 function createTaskCard() {
     const todoList = $('#todo-cards');
@@ -215,20 +131,28 @@ function createTaskCard() {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-    // ask}
+    $(".lane").droppable({
+        accept: ".dif",
+        drop: function(event, ui) {
+            handleDrop(event, ui); 
+        }
+    });
+
+    $(".dif").draggable({
+        helper: "clone",
+        revert: "invalid",
+        start: function(event, ui) {
+            ui.helper.addClass('dragging');
+        }
+    });
 }
 // Todo: create a function to handle adding a new task
 function handleDeleteTask(event) {
     const idDelete = event.target.getAttribute('data-project-id');
     let tasks = loadData();
 
-    // Filtramos las tareas para eliminar la que coincide con el id
     tasks = tasks.filter(task => task.id != idDelete);
-
-    // Guardamos la lista de tareas actualizada en localStorage
     saveLocal(tasks);
-
-    // Volvemos a crear las tarjetas
     createTaskCard();
 }
 
@@ -246,41 +170,17 @@ function handleDeleteTask(event) {
 
     saveLocal(tasks);
     createTaskCard();
-    // window.location.reload();
 }
-
-$(function() {
-    console.log("here");
-    
-    $(".lane").droppable({
-        accept: ".dif", // Acepta solo elementos con la clase .dif
-        drop: function(event, ui) {
-            handleDrop(event, ui); // Maneja el evento de caída
-        }
-    });
-});
-
-$(function() {
-    $(".dif").draggable({
-        helper: "clone", // Usa un clon del elemento arrastrable
-        revert: "invalid", // Revierte el arrastre si no es válido
-        start: function(event, ui) {
-            ui.helper.addClass('dragging'); // Opcional: añadir una clase durante el arrastre
-        }
-    });
-});
-
-
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
     const tasks = loadData();
-    const taskId = ui.draggable.data('project-id'); 
+    const taskId = ui.draggable.attr('data-project-id'); 
     const newStatus = $(event.target).attr('id'); 
 
     tasks.forEach(task => {
         if (task.id == taskId) {
-            task.status = newStatus; 
+            task.status = newStatus;
         }
     });
 
@@ -298,10 +198,27 @@ $(document).ready(function () {
 
 createTaskCard();
 
-$(function () {
+$(document).ready(function() {
+
+    $(".lane").droppable({
+        accept: ".dif",
+        drop: function(event, ui) {
+            handleDrop(event, ui); 
+        }
+    });
+
+    $(".dif").draggable({
+        helper: "clone",
+        revert: "invalid",
+        start: function(event, ui) {
+            ui.helper.addClass('dragging');
+        }
+    });
+
     $("#datepicker").datepicker({
         changeMonth: true,
         changeYear: true
     });
-});
 
+    createTaskCard();
+});
